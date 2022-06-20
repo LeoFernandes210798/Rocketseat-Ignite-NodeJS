7e11e56fce591ef54e25d63d00e988c366706b66
Leornardo Fernandes <leo.fernandes98@hotmail.com>
2022-06-19 23:35:46 -0300

    Primeiro projeto com NodeJS

---
+++ Primeiro projeto com NodeJS/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express()
+
+app.post("/account", (request, response) =>{
+    const {cpf, name} = request.body;
+
+    return 
+})
+
+
+
+
+
+
+
+
+
+app.listen(3333)
